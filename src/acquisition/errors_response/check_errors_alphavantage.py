db4753dafad21656a93adb56703cc5d053cb3ffb
ansev <asevis0705@gmail.com>
2020-02-12 09:50:12 +0100

    code cleaned

--- src/acquisition/errors_response/check_errors_alphavantage.py
+++ src/acquisition/errors_response/check_errors_alphavantage.py
@@ -1,26 +1,33 @@
 from src.acquisition.errors_response import check_errors
 from src.exceptions.acquisition_exceptions import AlphaVantageError
-from src.tools.mappers import map_dict_from_underscore,switch_None
+from src.tools.mappers import map_dict_from_underscore, switch_None
 
 class ErrorsResponseApiAlphavantage(check_errors.ErrorsResponseApi):
-
+    '''
+    This class generates exceptions related to the data acquired from the Alphavantage API
+    '''
     def __init__(self):
-        self._map_test = {'TIME': self._TimeSeries,
-                          'GLOBAL': self._StockTimeSeries_global,
-                          'SYMBOL': self._StockTimeSeries_symbol,
-                          'CURRENCY': self._Cryptocurrencis,
-                          'FX': self._TimeSeries,
-                          'DIGITAL': self._TimeSeries,
-                          'SECTOR': self._SectorPerformance,
+        self._map_test = {'TIME': self._time_series,
+                          'GLOBAL': self._stock_time_series_global,
+                          'SYMBOL': self._stock_time_series_symbol,
+                          'CURRENCY': self._cryptocurrencis,
+                          'FX': self._time_series,
+                          'DIGITAL': self._time_series,
+                          'SECTOR': self._sector_performance,
                           }
-        
+
     def empty_keys(self, key):
-        if key == '': raise AlphaVantageError('keys do not match the keys of the query',ValueError)
-        return False
+        if key == '':
+            raise AlphaVantageError('keys do not match the keys of the query', ValueError)
+
 
     def pass_test(self, json, query, decoded_function=None):
+        '''
+        This function maps the json format test function
+        and raises an exception if it returns True
+        '''
         json_keys = list(json)
-        map_function = switch_None(decoded_function,query['function'])
+        map_function = switch_None(decoded_function, query['function'])
         if map_dict_from_underscore(dict_to_map=self._map_test,
                                     function=map_function,
                                     n=0,
@@ -28,15 +35,20 @@ class ErrorsResponseApiAlphavantage(check_errors.ErrorsResponseApi):
 
             raise AlphaVantageError('json received invalid to type inquiry: {}'.format(query['function']),
                                     {'query': query, 'json_keys': json_keys})
-                                    
-    def _TimeSeries(self, json_keys): return (json_keys[0]!='Meta Data') or (len(json_keys)!=2)
 
-    def _StockTimeSeries_global(self, json_keys): return (json_keys[0]!='Global Quote') or (len(json_keys)!=1)
+    def _time_series(self, json_keys):
+        return (json_keys[0] != 'Meta Data') or (len(json_keys) != 2)
 
-    def _StockTimeSeries_symbol(self, json_keys): return (json_keys[0]!='bestMatches') or (len(json_keys)!=1)
-        
-    def _Cryptocurrencis(self, json_keys): return (json_keys[0]!='Realtime Currency Exchange Rate') or (len(json_keys)!=1)
-        
-    def _SectorPerformance(self, json_keys): return (json_keys[0]!= 'Meta Data') or (len(json_keys)!=11)
+    def _stock_time_series_global(self, json_keys):
+        return (json_keys[0] != 'Global Quote') or (len(json_keys) != 1)
+
+    def _stock_time_series_symbol(self, json_keys):
+        return (json_keys[0] != 'bestMatches') or (len(json_keys) != 1)
+
+    def _cryptocurrencis(self, json_keys):
+        return (json_keys[0] != 'Realtime Currency Exchange Rate') or (len(json_keys) != 1)
+
+    def _sector_performance(self, json_keys):
+        return (json_keys[0] != 'Meta Data') or (len(json_keys) != 11)
 
     
