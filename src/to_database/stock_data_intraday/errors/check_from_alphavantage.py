331828b0401d55a1d825f7daf8a36cae61bae594
ansev <asevis0705@gmail.com>
2020-02-19 14:08:29 +0100

    added packages and modules to save time series intraday in database

--- src/to_database/stock_data_intraday/errors/check_from_alphavantage.py
+++ src/to_database/stock_data_intraday/errors/check_from_alphavantage.py
@@ -3,21 +3,23 @@ from src.exceptions.acquisition_exceptions import AlphaVantageError
 from src.to_database.stock_data_intraday.errors.check_errors_intraday import CheckToDataBaseIntraday
 
 class CheckErrorsFromAlphaVantage(CheckToDataBaseIntraday):
-    frecuencies_intraday_in_api= []
+    frecuencies_intraday_in_api = []
     __ID_DOCUMENT = 'alphavantage'
     def __init__(self):
         super().__init__()
-        self.frecuencies_intraday_in_database = self.__get_supported_frequencies(document_id=self.__ID_DOCUMENT)
+        self.frecuencies_intraday_in_database = (
+            self.__get_supported_frequencies(document_id=self.__ID_DOCUMENT)
+            )
 
     def check_frecuency_in_api(self, frecuency):
-        if not self.__frecuency_supported(frecuency=frecuency, list_frecuencies=self.frecuencies_intraday_in_api):
-            raise ToDataBaseAlphaVantageError(f'Frequency not supported, frequencies supported are: {self.frecuencies_intraday_in_api}', ValueError)
+        if not self.__frecuency_supported(frecuency=frecuency,
+                                          list_frecuencies=self.frecuencies_intraday_in_api):
 
-    def check_frecuency_in_key_data(self, key_data, frecuency):
+            raise ToDataBaseAlphaVantageError(
+                f'Frequency not supported, frequencies supported are:'/
+                '{self.frecuencies_intraday_in_api}', ValueError)
+
+    @staticmethod
+    def check_frecuency_in_key_data(key_data, frecuency):
         if frecuency not in key_data:
             raise ToDataBaseAlphaVantageError(f'Incorrect frecuency in response', AlphaVantageError)
-
-
-
-
-
