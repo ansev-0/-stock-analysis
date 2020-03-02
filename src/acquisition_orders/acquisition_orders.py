5723a4423fd3cdb3e4a103cfb5d4e80971356e19
ansev <asevis0705@gmail.com>
2020-03-02 15:03:49 +0100

    improved

--- src/acquisition_orders/acquisition_orders.py
+++ src/acquisition_orders/acquisition_orders.py
@@ -7,23 +7,22 @@ class AcquisitionOrders(DataBase):
         self.collection=self.database[collection]
 
     def set_acquisition_api_orders(self, api, list_orders, **kwards):
-        self.collection.update_one(filter={'_id' : api},
+        return self.collection.update_one(filter={'_id' : api},
                                    update={'$set' : {'orders': list_orders}},
                                    upsert=True, 
                                    **kwards)
 
     def delete_acquisition_api_orders(self, api):
-        self.collection.delete_one(query={'_id' : api})
+        return self.collection.delete_one(query={'_id' : api})
 
     def delete_acquisition_orders(self):
-        self.collection.delete_many(query={})
+        return self.collection.delete_many(query={})
 
     def get_acquisition_api_orders(self, api):
         return self.collection.find_one(filter={'_id' : api},
-                                        projection={'orders':1})['orders']
+                                        projection={'orders' : 1})['orders']
     
     def get_acquisition_orders(self):
         return  [order['orders'] 
                  for order in self.collection.find(filter={},
-                                                   projection={'orders':1})]
-
+                                                   projection={'orders' : 1})]
