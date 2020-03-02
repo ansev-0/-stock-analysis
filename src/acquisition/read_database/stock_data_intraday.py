040aa09ceb0036b259edcba4600743d04be725b8
ansev <asevis0705@gmail.com>
2020-03-02 13:35:51 +0100

    cleanning code

--- src/acquisition/read_database/stock_data_intraday.py
+++ src/acquisition/read_database/stock_data_intraday.py
@@ -1,21 +1,37 @@
+from functools import reduce
+import pandas as pd
 from src.database.database import DataBase
 from src.read_database.errors.check_stock_data_intraday \
      import CheckErrorsGetStockDataIntraday1minFromDataBase
-from functools import reduce
-import pandas as pd
+
+
 
 class GetStockDataIntraday1minFromDataBase(DataBase):
 
     DATABASE_NAME = 'stock_data_intraday_1min'
+
     def __init__(self, format_output='dataframe'):
         super().__init__(name_database=self.DATABASE_NAME)
         self.func_transformer_dataframe = self.__get_function_transformer_dataframe(format_output)
         self.check_errors = CheckErrorsGetStockDataIntraday1minFromDataBase()
 
     def get(self, stock, start, end, **kwards):
-        dataframe=(
+
+        '''
+        This function return stock data from stock_data_intraday_1min data base
+        selecting between start and end date.
+        Format of output will be the one specified when creating the class.
+
+        Parameters
+        -----------
+        ---> stock: specify label (name) of stock data.
+        ---> start: str or pd.Timedelta,  specify the initial date (including this one).
+        ---> end: str or pd.Timedelta,  specify the end date (including this one).
+        '''
+
+        dataframe = (
             self.__build_dataframe(
-                dict_stock=self.__get_dict_from_database(stock, 
+                dict_stock=self.__get_dict_from_database(stock,
                                                          start=self.__get_datetime_database(start),
                                                          end=self.__get_datetime_database(end)),
                 start=start,
@@ -25,19 +41,16 @@ class GetStockDataIntraday1minFromDataBase(DataBase):
         return self.func_transformer_dataframe(dataframe=dataframe, **kwards)
 
     def __get_dict_from_database(self, stock, start, end):
-        return reduce(lambda x,y: dict(x,**y) ,
-                               self.database[stock].find(filter={'_id' : {'$gte' : start,
-                                                                        '$lte' : end}},
-                                                         projection={'_id' : 0}))
-
+        return reduce(lambda cum_dict, dict_new: dict(cum_dict, **dict_new),
+                      self.database[stock].find(filter={'_id' : {'$gte' : start,
+                                                                 '$lte' : end}},
+                                                projection={'_id' : 0}))
 
     def __get_function_transformer_dataframe(self, format_output):
-
-        if format_output=='dict':
+        if format_output == 'dict':
             return self.__get_dict_from_dataframe
-        else:
-            return lambda dataframe, **kwards: dataframe
-        
+        return lambda dataframe, **kwards: dataframe
+
     @staticmethod
     def __get_datetime_database(date):
         if isinstance(date, pd.Timedelta):
@@ -52,14 +65,5 @@ class GetStockDataIntraday1minFromDataBase(DataBase):
 
     @staticmethod
     def __get_dict_from_dataframe(dataframe, orient='index', **kwards):
-        dataframe.index=dataframe.index.astype(str)
+        dataframe.index = dataframe.index.astype(str)
         return dataframe.to_dict(orient=orient)
-
-stock=GetStockDataIntraday1minFromDataBase(format_output='dict').get(stock='TWTR', start='12-27-2019 09:35:00', end='01-13-2020 13:45:00')
-print(stock)
-
-
-
-
-        
-
