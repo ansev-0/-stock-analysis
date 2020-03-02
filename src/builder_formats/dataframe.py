00d35d3b38e47442cb994bea86ec45ff27fa5e57
ansev <asevis0705@gmail.com>
2020-03-02 14:14:22 +0100

    change name of parameter

--- src/builder_formats/dataframe.py
+++ src/builder_formats/dataframe.py
@@ -1,8 +1,8 @@
 import pandas as pd
-def build_dataframe_from_timeseries_dict(dataframe, format_output=None, datetime_index=True, ascending=None):
+def build_dataframe_from_timeseries_dict(dataframe, format_index=None, datetime_index=True, ascending=None):
     dataframe=pd.DataFrame.from_dict(dataframe, orient='index').astype(float)
     if datetime_index:
-        dataframe.index = pd.to_datetime(dataframe.index, format= format_output)
+        dataframe.index = pd.to_datetime(dataframe.index, format= format_index)
     if ascending is not None:
         return dataframe.sort_index(ascending=ascending)
     return dataframe
