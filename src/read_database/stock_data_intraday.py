39b5b1c5b18457df32f5fc2f26dcea0591a75edf
ansev <asevis0705@gmail.com>
2020-03-02 14:18:43 +0100

    added new packages

--- src/read_database/stock_data_intraday.py
+++ src/read_database/stock_data_intraday.py
@@ -1,12 +1,13 @@
 from src.database.database import DataBase
 from src.read_databse.errors.check_stock_data_intraday \
      import CheckErrorsGetStockDataIntraday1minFromDataBase
+from src.builders_formats.dataframe import build_dataframe_from_timeseries_dict
 from functools import reduce
 import pandas as pd
 class GetStockDataIntraday1minFromDataBase(DataBase):
 
     DATABASE_NAME = 'stock_data_intraday_1min'
-    def __init__(self, format_output):
+    def __init__(self, format_output='dataframe'):
         super().__init__(name_database=self.DATABASE_NAME)
         self.func_transformer_dataframe = self.__get_function_transformer_dataframe(format_output)
         self.check_errors = CheckErrorsGetStockDataIntraday1minFromDataBase()
@@ -45,9 +46,10 @@ class GetStockDataIntraday1minFromDataBase(DataBase):
 
     @staticmethod
     def __build_dataframe(dict_stock, start, end, format_index=None, **kwards):
-        dataframe = pd.DataFrame.from_dict(dict_stock, orient='index').astype(float)
-        dataframe.index = pd.to_datetime(dataframe.index, format=format_index)
-        return dataframe.sort_index().loc[start:end]
+        return build_dataframe_from_timeseries_dict(dataframe=dict_stock,
+                                                    datetime_index=True,
+                                                    format_index=format_index,
+                                                    ascending=True).loc[start:end]
 
     @staticmethod
     def __get_dict_from_dataframe(dataframe, orient='index', **kwards):
