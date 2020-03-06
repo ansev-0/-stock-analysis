d7c656efcab3da821379aa46fcb0503403da25d0
ansev <asevis0705@gmail.com>
2020-03-06 12:43:30 +0100

    new function added

--- src/tools/check_components.py
+++ src/tools/check_components.py
@@ -1,2 +1,13 @@
+
 def check_equal_lengths(*args):
     if len(set(map(len,args)))>1: raise ValueError
+
+def eval_type_argument(type_arg):
+    if isinstance(type_arg, str):
+        try:
+            type_arg = eval(type_arg)
+        except Exception:
+            pass
+    if type(type_arg) != type:
+        raise ValueError('Invalid type argument')
+    return type_arg
