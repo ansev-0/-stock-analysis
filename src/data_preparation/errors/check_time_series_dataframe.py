b1f05f7bd0d4b93cdf98e9feb68448c086a71964
ansev <asevis0705@gmail.com>
2020-03-06 12:45:11 +0100

    data preparation added

--- src/data_preparation/errors/check_time_series_dataframe.py
+++ src/data_preparation/errors/check_time_series_dataframe.py
@@ -5,8 +5,8 @@ class CheckSplitDataFrame:
     def check_datetime_index(index):
         if not isinstance(index, pd.DatetimeIndex):
             raise TypeError('The index must be instance of pd.DatetimeIndex')
-        
+
     @staticmethod
     def check_valid_format_output(format_output):
         if format_output not in [list, dict]:
-            raise TypeError('format output must be instance of dict or list')
+            raise TypeError('Format output must be instance of dict or list')
