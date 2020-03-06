ce4191e4bf2987be924b7bd42571e7a86c381aa2
ansev <asevis0705@gmail.com>
2020-03-06 13:38:20 +0100

    improved

--- src/data_preparation/errors/check_time_series_dataframe.py
+++ src/data_preparation/errors/check_time_series_dataframe.py
@@ -1,12 +1,14 @@
 import pandas as pd
-class CheckSplitDataFrame:
 
+class CheckSplitDataFrameByGroups:
+    @staticmethod
+    def check_valid_format_output(format_output):
+        if format_output not in [list, dict]:
+            raise TypeError('Format output must be instance of dict or list')
+
+class CheckSplitDataFrame:
     @staticmethod
     def check_datetime_index(index):
         if not isinstance(index, pd.DatetimeIndex):
             raise TypeError('The index must be instance of pd.DatetimeIndex')
 
-    @staticmethod
-    def check_valid_format_output(format_output):
-        if format_output not in [list, dict]:
-            raise TypeError('Format output must be instance of dict or list')
--- src/data_preparation/time_series_dataframe.py
+++ src/data_preparation/time_series_dataframe.py
@@ -1,38 +1,46 @@
 import pandas as pd
+from src.data_preparation.errors.check_time_series_dataframe import CheckSplitDataFrameByGroups
 from src.data_preparation.errors.check_time_series_dataframe import CheckSplitDataFrame
 from src.tools.check_components import eval_type_argument
 
-class SplitDataFrame:
-
+class SplitDataFrameByGroups:
     def __init__(self, format_output=list):
 
-        self.check_errors = CheckSplitDataFrame()
+        self.check_errors_bygroups = CheckSplitDataFrameByGroups()
         #get format_output like type instance
         self.__format_output = eval_type_argument(format_output)
         #eval format supported
-        self.check_errors.check_valid_format_output(self.__format_output)
+        self.check_errors_bygroups.check_valid_format_output(self.__format_output)
 
-    def by_discontinuous_index(self, dataframe, freq_limit):
-        #Check DateTimeIndex
-        self.check_errors.check_datetime_index(index=dataframe.index)
-        #Take blocks to use pandas.DataFrame.GroupBy
-        continuos_time_blocks = self.__get_blocks_continuous_periods(dataframe, freq_limit)
-        #Get output
-        return self.__split_groupby(dataframe=dataframe, by=continuos_time_blocks)
-    
-    def by_frecuency(self, dataframe, freq, **kwards):
-        return self.__split_groupby(dataframe=dataframe, by=pd.Grouper(freq=freq, **kwards))
-
-    def __get_blocks_continuous_periods(self, dataframe, freq):
-        return dataframe.index.to_series().diff().ge(pd.Timedelta(freq)).cumsum()
-
-    def __split_groupby(self, dataframe, by, **kwards):
-        groups = dataframe.groupby(by=by, **kwards)
+    def split(self, dataframe, *args, **kwards):
+        groups = dataframe.groupby(*args, **kwards)
 
         if self.__format_output == list:
             return [group for by, group in groups]
         else:
             return dict(groups.__iter__())
+
+class SplitDataFrame(SplitDataFrameByGroups):
+
+    def __init__(self, **kwards):
+        self.check_errors = CheckSplitDataFrame()
+        super().__init__(**kwards)
+
+    def by_discontinuous_index(self, dataframe, freq_limit):
+        #Check DateTimeIndex
+        self.check_errors.check_datetime_index(index=dataframe.index)
+        #Take blocks to use split function
+        continuos_time_blocks = self.__get_blocks_continuous_periods(dataframe, freq_limit)
+        #Get output
+        return self.split(dataframe=dataframe, by=continuos_time_blocks)
+    
+    def by_frecuency(self, dataframe, freq, **kwards):
+        return self.split(dataframe=dataframe, by=pd.Grouper(freq=freq, **kwards))
+
+    def __get_blocks_continuous_periods(self, dataframe, freq):
+        return dataframe.index.to_series().diff().ge(pd.Timedelta(freq)).cumsum()
+
+
 
         
 
