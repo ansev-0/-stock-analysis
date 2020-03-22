8e55ad8f12f89833b0ba5199101bbcd10b4aed52
ansev <asevis0705@gmail.com>
2020-03-22 19:36:59 +0100

    data prep improved

---
+++ src/data_preparation/nn_input_output.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+class BuilderIOStacked:
+
+    def __init__(self):
+        self.keys = ('x', 'y')
+
+    def dataframe_delays_from_serie(self, serie, delays):
+        return pd.concat([serie.shift(i).rename(f'Serie_delay_{i}') 
+                          for i in range(0, delays + 1)], axis=1).dropna()     
+
+    def input_output_from_dataframe_delays(self, dataframe, col_start_input):
+        return {by: group.to_numpy() 
+                for by, group in dataframe.groupby(by=np.where(
+                    np.arange(len(dataframe.columns)) >= col_start_input,
+                    *self.keys))}
+
---
+++ src/exceptions/data_preparation_exceptions.py
@@ -0,0 +1,3 @@
+from src.exceptions import new_exception
+class ScalerInputOutputError(new_exception.NewException):
+    pass
