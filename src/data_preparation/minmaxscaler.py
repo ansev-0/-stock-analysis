8fe047d0b6f060f229d88cf8f393e24770f9cf05
ansev <asevis0705@gmail.com>
2020-03-24 12:08:12 +0100

    improved

--- src/data_preparation/minmaxscaler.py
+++ src/data_preparation/minmaxscaler.py
@@ -1,24 +1,37 @@
 from sklearn.preprocessing import MinMaxScaler
+from functools import reduce
 
 class MinMaxScalerFitTransform:
-    def array(self, array, feature_range=(-1, 1), transform=True):
-        scaler = self._fit_scaler(array, feature_range=feature_range)
+
+    def __init__(self, feature_range):
+        self.feature_range = feature_range
+
+    def array(self, array, transform=True):
+        scaler = self._fit_scaler(array)
         if transform:
              array = scaler.transform(array)
         return scaler, array
 
-    def dict_array(self, dict_array, feature_range=(-1, 1),  transform=None):
-        if not transform:
-            transform = list(dict_array)
-
-        for key, array in dict_array.items():
-            dict_array[key] = dict(zip(('scaler', 'values'),
-                                       self.array(array,
-                                                  feature_range=feature_range,
-                                                  transform=key in transform)))    
-        return dict_array
-
-    def _fit_scaler(self, values, feature_range=(-1, 1)):
-        scaler = MinMaxScaler(feature_range=feature_range)
+    def _fit_scaler(self, values):
+        scaler = MinMaxScaler(feature_range=self.feature_range)
         scaler.fit(values)
         return scaler
+
+class MinMaxScalerFitTransformMany(MinMaxScalerFitTransform):
+
+
+    def tuple_array(self, tuple_array, transform=True, flatten=True):
+        mapper = map(*self.__map_args(tuple_array, transform))
+        if flatten: 
+            return reduce(lambda cum_tuple, new_tuple: cum_tuple + new_tuple, mapper)
+        return tuple(mapper)
+
+
+
+    def __map_args(self, tuple_array, transform):
+        if isinstance(transform, bool):
+                return lambda tup: self.array(tup, transform), tuple_array
+        return lambda tup: self.array(*tup),  zip(tuple_array, transform)
+
+
+
