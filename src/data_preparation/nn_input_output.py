d828c5dd4f534b7171caaba9a2e8be4b53e42869
ansev <asevis0705@gmail.com>
2020-03-26 12:24:11 +0100

    improved

--- src/data_preparation/nn_input_output.py
+++ src/data_preparation/nn_input_output.py
@@ -6,9 +6,9 @@ class BuilderIOStacked:
     def __init__(self):
         self.keys = ('x', 'y')
 
-    def dataframe_delays_from_serie(self, serie, delays):
+    def dataframe_delays_from_serie(self, serie, first_delay, last_delay):
         return pd.concat([serie.shift(i).rename(f'Serie_delay_{i}') 
-                          for i in range(0, delays + 1)], axis=1).dropna()     
+                          for i in range(first_delay, last_delay + 1)], axis=1).dropna()     
 
     def input_output_from_dataframe_delays(self, dataframe, col_start_input):
         return {by : group.to_numpy() 
