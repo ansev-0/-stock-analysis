3f8dbbacb533143339258a00aca8f7e6056b7b68
ansev <asevis0705@gmail.com>
2020-05-04 17:50:46 +0200

    improved

--- src/models/pytorch/tools/lstm.py
+++ src/models/pytorch/tools/lstm.py
@@ -4,17 +4,18 @@ import torch.nn as nn
 
 class LSTMStateful(nn.Module):
     
-    def __init__(self, input_size, hidden_layer_size, batch_size,
+    def __init__(self, input_size, hidden_size, batch_size,
                  bidirectional=False, **kwargs):
         
         super().__init__()
-        self._hidden_cell = None
+        self._hidden_state, self._hidden_cell = (None, None)
         self._batch_size = batch_size
-        self._hidden_layer_size = hidden_layer_size
+        self._hidden_size = hidden_size
         self._bidirectional = bidirectional
-        self.reset_hidden_cell()
-        self.lstm = nn.LSTM(input_size, hidden_layer_size,
+        self.lstm = nn.LSTM(input_size, hidden_size,
                             bidirectional=bidirectional, **kwargs)
+        self.reset_hidden_cell()
+        self.reset_hidden_state()
         
     @property
     def batch_size(self):
@@ -25,19 +26,27 @@ class LSTMStateful(nn.Module):
         return self._bidirectional
         
     @property
-    def hidden_layer_size(self):
-        return self._hidden_layer_size
+    def hidden_size(self):
+        return self._hidden_size
     
     @property    
     def hidden_cell(self):
         return self._hidden_cell
     
+    @property    
+    def hidden_state(self):
+        return self._hidden_state
+    
     def reset_hidden_cell(self):
-        self._hidden_cell = (torch.zeros(self.lstm.num_layers * self.bidirectional, 
-                                         self.batch_size, self.hidden_layer_size),
-                             torch.zeros(self.lstm.num_layers * self.bidirectional,
-                                         self.hidden_layer_size))     
+
+        self._hidden_cell = torch.zeros(self.lstm.num_layers * (self.bidirectional + 1), 
+                                         self.batch_size, self.hidden_size)
+        
+    def reset_hidden_state(self):
+        self._hidden_state = torch.zeros(self.lstm.num_layers * (self.bidirectional + 1), 
+                                        self.batch_size, self.hidden_size)   
         
     def forward(self, input_seq):
-        lstm_out, self._hidden_cell = self.lstm(input_seq, self._hidden_cell)
-        return lstm_out
+        lstm_out, (self._hidden_cell, self._hidden_state) = self.lstm(input_seq, 
+                                                                      (self._hidden_cell, self._hidden_state))
+        return lstm_out, (self._hidden_cell, self._hidden_state)
