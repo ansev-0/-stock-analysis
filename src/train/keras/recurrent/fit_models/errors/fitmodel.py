36e65101fae3c67f58a4869f2186d54424b91c10
ansev-0 <you@example.com>
2020-07-08 20:04:35 +0200

    added admin

--- src/train/keras/recurrent/fit_models/errors/fitmodel.py
+++
@@ -1,9 +0,0 @@
-from src.exceptions.train_exceptions import ModelFitError
-class CheckFitModelWithState:
-    @staticmethod
-    def check_history_keys(history_keys):
-        try:
-            iter(history_keys)
-        except TypeError:
-            raise   ModelFitError('You must pass a iter')
-
