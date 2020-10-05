f3b92a96ae783fc87efec3af2ab02a74dafc0c7e
ansev <ansev0705@gmail.com>
2020-10-05 14:58:13 +0200

    improved

--- src/model_environment/rewards/dynamic/builder_dict_rewards.py
+++ src/model_environment/rewards/dynamic/builder_dict_rewards.py
@@ -11,20 +11,3 @@ class BuilderDictRewards(BuildDictNode):
                            **{DynamicDictNodeBuilder(type_reward).build(module_obj_dict) 
                               for type_reward, module_obj_dict in params.items()})
 
-dict_rewardnode_not_depend_on_inventory = {'weight' : 0.9, 'bias' : 0.2}
-dict_rewardnode_depend_on_inventory = {'weight' : 1.0, 'bias' : 0.2}
-
-dict_not_depend_on_inventory = {'monotic_rewards' : {'MonoticCumulativeRewards' : {'time_values' :range(19)}},
-
-                               'rewardnode' : dict_rewardnode_not_depend_on_inventory}
-
-dict_depend_on_inventory = {'mean_rewards' : {'LastPurchasesMeanReward' : {'init_mean_inventory' :0,
-                                                                          'init_n_stocks' : 0}},
-
-                               'rewardnode' : dict_rewardnode_not_depend_on_inventory}
-
-dict_rewardnode = {'weight' : 1.0, 'bias' : 0.2}
-dict_rewards = {'not_depend_on_inventory' : dict_not_depend_on_inventory,
-                'depend_on_inventory' : dict_depend_on_inventory,
-                'rewardnode' : dict_rewardnode}
-
