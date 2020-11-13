6ffb94cdd5886de87ccb6a3c46cbb6ee3f37aad0
ansev-0 <you@example.com>
2020-11-13 17:35:25 +0100

    improved

--- src/model_environment/rewards/not_depend_on_inventory/rewards/base_rewards.py
+++ src/model_environment/rewards/not_depend_on_inventory/rewards/base_rewards.py
@@ -8,26 +8,19 @@ class BaseNotDependOnInventoryReward(NotDependOnInventoryReward, metaclass=ABCMe
 
     def __init__(self, 
                  time_values=None,
-                 id_cache=None, 
-                 commision=0, 
-                 gamma_pos_not_actions=1, 
+                 commision=None, 
                  rewardnode=None):
                  
         super().__init__(rewardnode)
-        self._id_cache = id_cache
+
         self._commision = commision
         self._mapper_action_rewards = None
-        self._time_values = self._init_time_values(time_values)
-        self._gamma_pos_not_actions = gamma_pos_not_actions
+        self._time_values = time_values
         self._get_mapper_action_rewards()
 
     @abstractmethod
     def _get_sell_serie(self):
         pass
-
-    @property
-    def id_cache(self):
-        return self._id_cache
 
     @property
     def mapper_action_rewards(self):
@@ -46,14 +39,6 @@ class BaseNotDependOnInventoryReward(NotDependOnInventoryReward, metaclass=ABCMe
     def commision(self):
         return self._commision
 
-    @commision.setter
-    def commision(self, commision):
-        self._commision = commision if commision is not None else 0
-        self._get_mapper_action_rewards()
-
-    @property
-    def gamma_pos_not_actions(self):
-        return self._gamma_pos_not_actions
 
     @gamma_pos_not_actions.setter
     def gamma_pos_not_actions(self, gamma_pos_not_actions):
@@ -64,27 +49,17 @@ class BaseNotDependOnInventoryReward(NotDependOnInventoryReward, metaclass=ABCMe
 
         sell = self._get_sell_serie()
         buy = sell.mul(-1).rename('buy_rewards')
+        mapper = self._get_dict_mapper(sell, buy)
 
-        no_actions_serie = sell.abs().mul(-1).add(self.commision).rename('commision_rewards')
-        buy -= self.commision
-        sell -= self.commision
-        no_actions_serie = no_actions_serie.where(no_actions_serie.le(0),
-                                                  no_actions_serie.mul(self.gamma_pos_not_actions))
-        self._mapper_action_rewards = self._get_dict_mapper(no_actions_serie.values, sell.values, buy.values)
+        self._mapper_action_rewards = lambda action, time, n_stocks: mapper[action] - \
+            self._commision(n_stocks=n_stocks, 
+                            stock_price=self.time_values[time], 
+                            time=time) if action != 'no_action' else 0
+
+
+    
 
     @staticmethod
     def _get_dict_mapper(*args):
-        return dict(zip(('no_action', 'sell', 'buy'),
+        return dict(zip(('sell', 'buy'),
                         args))
-
-    def _init_time_values(self, time_values):
-
-        if self._id_cache is not None:
-            return pd.Series(FindAgentTrainCache().\
-                find_by_id(self._id_cache, 
-                           projection = {'time_values' : True,
-                                         '_id' : False})['time_values'])
-        elif time_values is not None:
-            return time_values
-
-        raise ValueError('You must pass time_values or id_cache parameters')
--- src/model_environment/rewards/not_depend_on_inventory/rewards/monotic_rewards.py
+++ src/model_environment/rewards/not_depend_on_inventory/rewards/monotic_rewards.py
@@ -8,55 +8,6 @@ class MonoticCumulativeRewards(BaseNotDependOnInventoryReward):
         blocks = monotic_blocks(diff, diff_serie=True)
         return diff[::-1].groupby(blocks, sort=False).cumsum()[::-1].dropna().rename('sell_rewards')
 
-class LossOpportunityPenaltyRewards(MonoticCumulativeRewards):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._penalty_reward = self._time_values.diff(-1).dropna().values
-        self._buy_cum_rewards = self.mapper_action_rewards['buy']
-
-    def get_reward(self, time, max_purchases, *args, **kwargs):
-
-        try:
-            return self.rewardnode(self._get_penalty(time, max_purchases)) 
-#
-        except KeyError as error:
-            keys_str = ' or '.join(self.mapper_action_rewards.keys())
-            raise KeyError(error, f'You must pass : {keys_str}' )
-
-
-    def _get_penalty(self, time, max_purchases):
-        return max_purchases * self._penalty_reward[time] \
-            if self._necessary_unit_penalty(time) else 0
-
-    def _necessary_unit_penalty(self, time):
-        return self._buy_cum_rewards[time] > 0 
-
-
-class SellRewards(MonoticCumulativeRewards):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._sell_rewards = self._time_values.diff(-1).dropna().values
-        self._sell_cum_rewards = self.mapper_action_rewards['sell']
-
-    def get_reward(self, action, time, n_stocks, *args, **kwargs):
-
-        try:
-            return self.rewardnode(self._get_sell_reward(action, time, n_stocks)) 
-#
-        except KeyError as error:
-            keys_str = ' or '.join(self.mapper_action_rewards.keys())
-            raise KeyError(error, f'You must pass : {keys_str}' )
-
-
-    def _get_sell_reward(self, action, time, n_stocks):
-        return n_stocks * self._sell_rewards[time] \
-            if ('action' == 'sell') and (self._sell_cum_rewards[time] > 0) else 0
-
-
-
-
         
 class MonoticCumulativeRewardsNotAction(MonoticCumulativeRewards):
     def get_reward(self, action, time, *args):
