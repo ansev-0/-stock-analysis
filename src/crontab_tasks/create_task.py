b0a7ac2b86cdc58d7154c889164ed144542f9194
ansev <ansev0705@gmail.com>
2020-11-18 18:12:29 +0100

    improved

--- src/crontab_tasks/create_task.py
+++ src/crontab_tasks/create_task.py
@@ -1,13 +1,12 @@
 from src.crontab_tasks.crontab_task import TaskCronTab
-
+from datetime import datetime 
 class CreateRunTaskCronTab(TaskCronTab):
 
     def add_task(self, module, **kwargs):
         return super().new(command=self._build_path(module), **kwargs)
 
-
     def add_daily_task(self, module, **kwargs):
         job = self.add_task(module, **kwargs)
-        job.day.every(1)
+        job.every(1).days()
 
         return job
